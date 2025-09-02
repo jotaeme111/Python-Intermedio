8616308f3d0be573b0c4ea9764be01b1a2d18a0d
Jorge Mario Jimenez <jorgejimenezporras@outlook.com>
2025-09-01 21:11:37 -0600

    Update ejemplo_tk_inter.py

--- ejemplo_tk_inter.py
+++ ejemplo_tk_inter.py
@@ -24,12 +24,13 @@ entry_nombre.pack(pady=10) #Empaqueta la entrada de texto en la ventana
 boton_saludar = tk.Button(root, text="Saludar", command=mostrar_saludo, font=("Arial", 12), fg="white", bg="green") #Botón que al hacer clic llama a la función mostrar_saludo.
 boton_saludar.pack(pady=10) #Empaqueta el botón en la ventana
 
-# #Crear una etiqueta (label) para mostrar texto en la ventana.
-# label = tk.Label(root, text="¡Hola, Bienvenido a mi app!", font=("Arial", 16), fg="blue", bg="white") #Creando una etiqueta. #root es el padre de la etiqueta, lo envia a la etiqueta principal.
-# label.pack(pady=20) #Empaqueta la etiqueta en la ventana con un margen vertical de 20 píxeles.
+#Crear una etiqueta (label) para mostrar texto en la ventana.
+label = tk.Label(root, text="¡Hola, Bienvenido a mi app!", font=("Arial", 16), fg="blue", bg="white") #Creando una etiqueta. #root es el padre de la etiqueta, lo envia a la etiqueta principal.
+label.pack(pady=20) #Empaqueta la etiqueta en la ventana con un margen vertical de 20 píxeles.
 
-# #Crear un botón que al hacer clic llame a la función saludar.
-# button = tk.Button(root, text="Haz clic para saludar", command=saludar, font=("Arial", 12), fg="white", bg="blue") #Creando un botón. Al hacer clic en el botón, se ejecuta la función saludar.
-# button.pack(pady=10) #Empaqueta el botón en la ventana con un margen vertical de 10 píxeles.
+#Crear un botón que al hacer clic llame a la función saludar.
+button = tk.Button(root, text="Haz clic para saludar", command=saludar, font=("Arial", 12), fg="white", bg="blue") #Creando un botón. Al hacer clic en el botón, se ejecuta la función saludar.
+button.pack(pady=10) #Empaqueta el botón en la ventana con un margen vertical de 10 píxeles.
 
 root.mainloop() #Para mostrar la ventana. Es un bucle infinito que mantiene la ventana abierta hasta que se cierre.
+
